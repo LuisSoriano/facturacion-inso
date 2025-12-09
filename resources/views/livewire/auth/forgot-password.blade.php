cfa82caefb9fce889d99491f59a2f338f0d8499b
root <root@inso-server.inso.gob.bo>
2025-12-09 18:36:22 -0400

    First Commit

---
+++ resources/views/livewire/auth/forgot-password.blade.php
@@ -0,0 +1,31 @@
+<x-layouts.auth>
+    <div class="flex flex-col gap-6">
+        <x-auth-header :title="__('Olvidó su contraseña')" :description="__('Ingrese su correo electrónico para recibir un enlace de restablecimiento de contraseña')" />
+
+        <!-- Session Status -->
+        <x-auth-session-status class="text-center" :status="session('status')" />
+
+        <form method="POST" action="{{ route('password.email') }}" class="flex flex-col gap-6">
+            @csrf
+
+            <!-- Email Address -->
+            <flux:input
+                name="email"
+                :label="__('Correo Electrónico')"
+                type="email"
+                required
+                autofocus
+                placeholder="correo@ejemplo.com"
+            />
+
+            <flux:button variant="primary" type="submit" class="w-full" data-test="email-password-reset-link-button">
+                {{ __('Enviar enlace para restablecer contraseña') }}
+            </flux:button>
+        </form>
+
+        <div class="space-x-1 rtl:space-x-reverse text-center text-sm text-zinc-400">
+            <span>{{ __('O, regresar a') }}</span>
+            <flux:link :href="route('login')" wire:navigate>{{ __('iniciar sesión') }}</flux:link>
+        </div>
+    </div>
+</x-layouts.auth>
